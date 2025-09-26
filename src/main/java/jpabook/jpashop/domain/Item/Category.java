6c4de00a897581e9c8464cc3788fbce2da221005
M-roaroa <hyh1216@naver.com>
2025-09-26 15:48:00 +0900

    feat: Add Entity Class

---
+++ src/main/java/jpabook/jpashop/domain/Item/Category.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.domain.Item;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item", // 중간 테이블 (조인테이블), JoinTable => 실무에서는 사용하지 x, 실습이므로 진행
+        joinColumns = @JoinColumn(name = "category_id"), // 카테고리에 들어가는
+        inverseJoinColumns = @JoinColumn(name = "item_id")) // 아이템 쪽에 들어가는
+    private List<Item> items = new ArrayList<>();
+
+
+    //NOTE: 셀프로 양방향 연관관계 매핑
+    //내 부모가 내 타입
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded //내장 타입을 포함했다
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // Order의 member가 연관관계 주인, 읽기 전용으로 셋팅
+    private List<Order> orders = new ArrayList<>();
+
+}
